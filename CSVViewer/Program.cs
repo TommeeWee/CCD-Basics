1c6b85c6ab12785cc9b1f019be2c4271777b67ce
Tom W <tomwestphal@googlemail.com>
2023-02-15 11:44:53 +0100

    Projekt für CSV-Viewer angelegt

---
+++ CSVViewer/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public static class Programm
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Test");
+    }
+}
